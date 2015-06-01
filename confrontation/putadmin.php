8230ebab83a1f8d39dd4f91108306a9e1102d4ce
coooola <nicolas.bitan@free.fr>
2015-06-01 21:21:37 +0100

    Revert "Première et dernière version"
    This reverts commit 8a6feadfafd2b1eb2864e7677aeae7379755c6fc.

--- confrontation/putadmin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	include 'connexionBase.php';
-	$req="UPDATE \"FindMentor\".\"Joueur\" SET admin='TRUE' WHERE numjoueur='".$_POST['numJ']."'; ";
-	$reponse = $BDD->query($req);
-	header( "Location: panneauadmin.php" ); 
-	echo 'Confrontation créée <br /> <br /> Vous allez être redirigé dans environ 5 secondes. Si rien ne se passe, cliquez <a href="index.php">ici</a>.';
-?>
